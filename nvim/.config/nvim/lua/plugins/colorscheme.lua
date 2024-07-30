dae11a8db4565d2be1775c115ab324312104523c
Mr TripleA <kraaakilo@gmail.com>
2024-07-30 11:53:01 +0100

    Chore : Change tmux statusline && alias for zsh.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,12 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true
+    "rose-pine/neovim",
+    name = "rose-pine",
   },
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   name = "catppuccin",
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       background = {
-  --         dark = "macchiato",
-  --       },
-  --       integrations = {
-  --         neotree = false
-  --       }
-  --     })
-  --   end
-  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "rose-pine",
     },
   }
 }
